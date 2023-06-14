4fe68b09c86ddf5b2a58d83258f03d552c61494e
mayab <mayab@qwilt.com>
2023-06-15 00:37:07 +0300

    print things in worker

--- worker.py
+++ worker.py
@@ -26,6 +26,7 @@ def kill_me():
 def do_work(job):
     job_dict = json.loads(job)
     result = work(job_dict['text'], job_dict['iters'])
+    print(f"result {result} for job: {job_dict}")
     requests.post(f'http://{parent_dns}:5000/finishedWork', params={'jobId': job_dict['jobId'], 'result': result})
 
 
@@ -33,6 +34,7 @@ def get_work():
     global parent_dns, other_dns
     last_time = datetime.now().timestamp()
     while (datetime.now().timestamp() - last_time) <= 600:
+        print("Trying to get work")
         job = requests.get(f'http://{parent_dns}:5000/getWork').json()
         if 'jobId' in job:
             do_work(job)
@@ -41,6 +43,7 @@ def get_work():
             if 'jobId' in job:
                 do_work(job)
         time.sleep(15)
+    print("Killing Instance, Bye")
     kill_me()
 
 
